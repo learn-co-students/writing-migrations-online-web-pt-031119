7a1562e9a92eac0f63bae22187d92a0b4b6ef8cd
AJ Guiles <ashiguiles@gmail.com>
2019-06-21 02:20:02 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,12 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+# As of ActiveRecord 5.0,inheritance requires ActiveRecord verions. 
+
+class CreateStudents < ActiveRecord::Migration[5.1]  
+  
+def change
+  create_table :students do |t|
+    t.string :name
+  end
 end
+
+end
+  
